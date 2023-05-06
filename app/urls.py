e9fe11045ac19a934151dc72581ea46dbcd1dc8a
Sandeep Shaw <sandeep10shaw@gmail.com>
2023-05-07 00:12:01 +0530

    Template added

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="home"),
+    path("about/", views.index, name="about"),
+    path("blog/", views.index, name="blog"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,13 +1,10 @@
-from django.http import HttpResponse
-from core.utils.user import allUsers
-
+from django.shortcuts import render
 
 def index(request):
-    data = allUsers()
-    if(data['error']):
-        return HttpResponse(f"Error fetching user: {data['error']}")
-    else:
-        names = ''
-        for i in data['users']:
-            names += f"{i} "
-        return HttpResponse(f"User: {names}")
+    links = [
+        { 'name': 'Home', 'url': 'home'},
+        { 'name': 'About', 'url': 'about'},
+        { 'name': 'Blog', 'url': 'blog'}
+    ]
+    context = { 'links': links, 'request': request }
+    return render(request,'home.html', context)
