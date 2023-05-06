2d669bd75e4b86bedc7f0ec151f3e810b0aae564
Sandeep Shaw <sandeep10shaw@gmail.com>
2023-05-06 21:25:18 +0530

    Adding Static Files

--- core/settings.py
+++ core/settings.py
@@ -128,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'static'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" #dependency whitenoise
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
