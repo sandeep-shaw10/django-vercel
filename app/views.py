2ad520b50a965333ad882f89b6aa672756372277
Sandeep Shaw <sandeep10shaw@gmail.com>
2023-05-08 01:48:55 +0530

    Pages Data Added

--- app/views.py
+++ app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+import random
 
 def index(request):
     links = [
         { 'name': 'Home', 'url': 'home'},
         { 'name': 'About', 'url': 'about'},
+        { 'name': 'Experience', 'url': 'experience'},
+        { 'name': 'Project', 'url': 'project'},
         { 'name': 'Blog', 'url': 'blog'}
     ]
-    context = { 'links': links, 'request': request }
+    project = ['1', '2', '3', '4', '5', '6']
+    blog = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
+    blog = random.sample(blog, 6)
+    context = { 'links': links, 'request': request, 'project': project, 'blog': blog }
     return render(request,'index.html', context)
