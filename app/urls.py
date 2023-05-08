eab2af309b24d078a177952de1446ad0336d293b
Sandeep Shaw <sandeep10shaw@gmail.com>
2023-05-09 00:00:01 +0530

    Add routing Structure

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("blog/", views.blog, name="blogs"),
+    path("blog/<slug>", views.getBlog, name="get_blog"),
 ]
--- app/views.py
+++ app/views.py
@@ -14,3 +14,18 @@ def index(request):
     blog = random.sample(blog, 6)
     context = { 'links': links, 'request': request, 'project': project, 'blog': blog }
     return render(request,'index.html', context)
+
+
+def blog(request):
+    blog = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
+    featured = random.sample(blog, 3)
+    context = { 'blog': blog, 'featured': featured }
+    return render(request,'pages/blog/index.html', context)
+
+
+def getBlog(request, slug):
+    if(slug == '1'):
+        context = { 'slug': slug }
+        return render(request,'pages/blog/blog.html', context)
+    else:
+        return render(request,'404.html')
